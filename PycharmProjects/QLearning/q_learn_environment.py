c6debca08baaed205d2171778f602ceb68d7439a
Kristian Thaulow <kristian1990@gmail.com>
2024-04-17 09:21:50 +0200

    Update comments

--- PycharmProjects/QLearning/q_learn_environment.py
+++ PycharmProjects/QLearning/q_learn_environment.py
@@ -20,6 +20,10 @@ class QLearnEnvironment:
     actions = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 
     # Defining the rewards
+    # Rows are locations and columns are actions
+    # E.g. row 0 is state A.
+    # The columns define that it can only transition to index 1, which is B
+
     R = np.array([[0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                   [1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0],
                   [0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
