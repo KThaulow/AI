837c95a8c43dd5ade3ba25fb3014f0b9d8eb826a
Kristian Thaulow <kristian1990@gmail.com>
2024-04-19 10:30:20 +0200

    Training the agent

--- PycharmProjects/DeepQLearning_LunarLanding/DqnAgent.py
+++ PycharmProjects/DeepQLearning_LunarLanding/DqnAgent.py
@@ -1,4 +1,29 @@
+from collections import deque
+
 from Agent import Agent
-from AgentTrainer import state_size, number_actions
+from AgentTrainer import state_size, number_actions, env
 
 agent = Agent(state_size, number_actions)
+
+number_episodes = 2000
+maximum_number_timesteps_per_episode = 1000
+epsilon_starting_value = 1.0
+epsilon_ending_value = 0.01
+epsilon_decay_value = 0.995
+epsilon = epsilon_starting_value
+scores_on_100_episodes = deque(maxlen=100)
+
+for episode in range(1, number_episodes + 1):
+    state, _ = env.reset()
+    score = 0
+    for t in range(maximum_number_timesteps_per_episode):
+        action = agent.act(state, epsilon)
+        next_state, reward, done, _, _ = env.step(action)
+        agent.step(state, action, reward, next_state, done)
+        state = next_state
+        score += reward
+        if done:
+            break
+
+    scores_on_100_episodes.append(score)
+    epsilon = max(epsilon_ending_value, epsilon_decay_value * epsilon)
